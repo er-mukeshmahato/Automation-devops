a79e795d31b21c96034029b4cabb15e240106f93
Mukesh Kr Mahato <53220246+er-mukeshmahato@users.noreply.github.com>
2025-01-31 14:58:15 +0900

    updated

--- banking-api/src/main/java/com/banking/api/banking_api/constant/roleConstants.java
+++
@@ -1,9 +0,0 @@
-package com.banking.api.banking_api.constant;
-
-public class roleConstants {
-
-    public static final int User = 1;
-    public static final int Admin = 2;
-    public static final int Master = 3;
-
-}
