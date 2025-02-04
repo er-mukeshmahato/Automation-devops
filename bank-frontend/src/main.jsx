c08cac89083f12491104113083ced931025d8094
Mukesh Kr Mahato <53220246+er-mukeshmahato@users.noreply.github.com>
2025-02-04 23:21:47 +0900

    updated css

--- bank-frontend/src/main.jsx
+++ bank-frontend/src/main.jsx
@@ -5,7 +5,6 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import './index.css'
 import App from './App'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import Dashboard from './Dashboard'
 import { BrowserRouter } from 'react-router-dom'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
